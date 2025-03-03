88adba42253595f323add0ef3bef28f92e2bdab0
rachelyr <rachel.yorke225@gmail.com>
2025-03-04 00:51:47 +0530

    font and post css config
    font style changed and tailwind setup files

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        header: '560px'
+      },
+      colors:{
+        main: '#ed4c07',
+        subMain: '#e05012',
+        deepGray: '#fff7f3',
+        dryGray: '#F3F4F6',
+        flash: '#FF1E1E',
+        star: '#FFB000',
+        deepest: '#fce6dd',
+        text: 'C0C0C0',
+        overlay: '#00000010'
+      },
+      fontSize:{
+        h1: '2.6rem'
+      },
+      screens: {
+        xs: '475px'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar-hide'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
